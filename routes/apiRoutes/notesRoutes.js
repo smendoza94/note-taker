efdc1c561cd7c361d695e426cf4e908e12dc56c2
StevenM <stevenbmendoza@gmail.com>
2022-04-14 21:51:00 -0700

    MVP & routes

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { 
+  filterByQuery, 
+  findById, 
+  createNewNote, 
+  validateNote } = require('../../lib/notes');
+const { db } = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+router.post('/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  if (!validateNotes(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports = router;
